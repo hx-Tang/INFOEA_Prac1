aa8d4092aa37b542a128e8bc8d93d280dc964e5c
YueShi12 <92110708+YueShi12@users.noreply.github.com>
2022-03-11 14:54:44 +0100

    Update Evaluation.py

--- Evaluation.py
+++ Evaluation.py
@@ -34,12 +34,16 @@ def err_and_corr(M1,M2):
 def average_and_std(P):
     fit_all_1=[]
     fit_all_0=[]
+    num_0=0
+    num_1=0
     for instance in P:
         fi =instance[1]
         body =instance[0]
         if body[0] == 1:
+            num_1 =num_1 + 1
             fit_all_1.append(fi)
         else:
+            num_0 = num_0 + 1
             fit_all_0.append(fi)
     if len(fit_all_1) == 0:
         aver_1, std_1 =0,0
@@ -51,7 +55,7 @@ def average_and_std(P):
     else:
         aver_0 =sum(fit_all_0)/len(fit_all_0)
         std_0 = std(fit_all_0)
-    return aver_1,std_1,aver_0,std_0
+    return num_1, num_0, aver_1,std_1,aver_0,std_0
 
 def average_fi(P):
     fi=[]
